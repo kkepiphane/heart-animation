3d2e3b2c68e3e724d60727343021fec14f7e40b6
kkepiphane <ekoutsava@gmail.com>
2024-08-07 07:37:34 +0000

    add range silder

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,15 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double ratings = 1; // Définir ratings comme une variable double
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +21,31 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.grey,
-        body: Center(
-          child: CircleCustomPain(
-            durationInSeconds: 120,
-          ),
+        body: Stack(
+          children: [
+            // Positionner CircleCustomPain pour occuper tout l'espace
+            Positioned.fill(
+              child: CircleCustomPain(
+                durationInSeconds:
+                    100 - ratings.toInt(), // Convertir en int si nécessaire
+              ),
+            ),
+            Positioned(
+              top: 90,
+              left: 0,
+              right: 0,
+              child: Slider(
+                value: ratings,
+                onChanged: (newRating) {
+                  setState(() => ratings = newRating);
+                },
+                min: 1,
+                max: 100,
+                divisions: 99, // Optionnel : pour avoir des valeurs entières
+                label: "$ratings",
+              ),
+            ),
+          ],
         ),
       ),
     );
